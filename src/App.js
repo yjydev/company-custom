bc93945372cb34689a3598bcda5849b0c9781229
jayjayKimm <jeongjinkim@zinnotech.com>
2022-10-21 17:23:09 +0900

    server connection test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
-
+import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
+import Start from "./Wonik/Start";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Start />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/Wonik/Start.js
+++ src/Wonik/Start.js
@@ -1,14 +1,86 @@
+import axios from "axios";
 import React from "react";
 
+const MENU = "__ZINNOHUB_MENU__";
+const TOKEN = "__ZINNOHUB_TOKEN__";
+const REFRESH_TOKEN = "__ZINNOHUB_REFRESHTOKEN__";
+
 function Start(props) {
-  const handleEmail = () => {};
-  const handlePassword = () => {};
-  const handleSubmit = () => {};
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = async () => {
+    const formData = {
+      email: email.toLowerCase().trim(),
+      password: password.trim(),
+      module_name: "zinnoHUB",
+      // ip_address: ipAddress.data,
+      mac_address: null,
+    };
+
+    const request = await axios.put(
+      "http://localhost:3000/api/login_v2",
+      formData
+    );
+
+    const { is_success, message, token, refresh_token, feature_list } =
+      request.data;
+
+    if (is_success) {
+      setToken(token);
+      setRefreshToken(refresh_token);
+      setMenu(feature_list);
+
+      window.location.href = "http://localhost:3000";
+    }
+  };
+
+  const setToken = (value) => {
+    let settingValue;
+
+    if (value) {
+      settingValue = value;
+    } else {
+      settingValue = "";
+    }
+
+    localStorage.setItem(TOKEN, settingValue);
+  };
+
+  const setRefreshToken = (token) => {
+    let settingValue;
+
+    if (token) {
+      settingValue = token;
+    } else {
+      settingValue = "";
+    }
+
+    localStorage.setItem(REFRESH_TOKEN, settingValue);
+  };
+
+  const setMenu = (value) => {
+    let settingValue;
+
+    if (value) {
+      settingValue = JSON.stringify(value);
+    } else {
+      settingValue = "";
+    }
+
+    localStorage.setItem(MENU, settingValue);
+  };
 
   return (
     <div>
-      <input type="text" onChange={handleEmail} />
-      <input type="password" onChange={handlePassword} />
+      <input type="text" value={email} onChange={handleEmail} />
+      <input type="password" value={password} onChange={handlePassword} />
       <button onClick={handleSubmit}>Sign In</button>
     </div>
   );
